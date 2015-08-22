1133aa7b26eb221bf9cf44449ea5e747944c96cf
weizs <weizs@mysoft.com.cn>
2015-08-22 18:09:48 +0800

    init

---
+++ game.js
@@ -0,0 +1,128 @@
+/**
+ * Created by weizs on 2015/8/21.
+ */
+
+
+(function(){
+
+    var defaultOptions={
+            callback:function(){},
+            width:60,
+            height:36,
+            xMin:0,
+            yMin:0,
+            size:5,
+            wrapWidth:800,
+            wrapHeight:1200,
+            minDelay:300,
+            maxDelay:800,
+            animateGap:800//TODO
+        },
+        animate=[],
+        indexCache=[],
+        position={},
+        count=1;
+
+    var HB=function(options){
+        this.options= $.extend({},defaultOptions,options);
+        this.init();
+    };
+
+    HB.prototype={
+        init:function(){
+            var _self=this;
+            _self.wrap=$(_self.options.wrap);
+            _self.wrap.on('click','.hb',function(){
+                var $this=$(this);
+                _self.stop();
+                _self.options.callback.call(this,$this,$this.attr('id').split('_')[1]);
+            });
+        },
+        stop:function(){
+            this.animate=false;
+            while(animate.length) clearTimeout(animate.shift());
+            this.wrap.find('.hb').remove();
+        },
+        play:function(){
+            var _self=this;
+            console.log('play');
+            _self.animate=true;
+            for(var i=0;i<_self.options.size;i++){
+                setTimeout(function(){
+                    _self.show();
+                },_self.getDelay(true));
+            }
+        },
+        show:function(){
+            if(!this.animate)return;
+            var _self=this;
+            var pos = _self.getPos(),
+                object = _self.getDom(pos);
+
+            _self.wrap.append(object.dom);
+
+            animate[object.index]=setTimeout(function(){
+                _self.hide(object);
+            },_self.getDelay());
+        },
+        hide:function(object){
+            object.dom.hide();
+            var index=indexCache.indexOf(object.id);
+            if(index!=-1){
+                var val=indexCache.splice(index,1)[0];
+                delete position[val];
+            }
+            if(!indexCache.length&&this.animate){
+                this.animate=false;
+                this.play();
+            }
+            object.dom.remove();
+        },
+        getDom:function(pos){
+            var index=count++,
+                opt=this.options;
+            pos.index=index;
+            position[index]=pos;
+            indexCache.push(index);
+            return {
+                dom:$('<div id="id_'+index+'" class="hb" style="left:'+pos.x+'px;top:'+pos.y+'px;width:'+opt.width+'px;height:'+opt.height+'px;line-height:'+opt.height+'px;">点我</div>'),
+                id:index
+            };
+        },
+        getDelay:function(begin){
+            var opt=this.options,
+                time=opt.minDelay+parseInt((opt.maxDelay-opt.minDelay)*Math.random());
+            return begin?opt.maxDelay-time:time;
+        },
+        getPos:function(){
+            var opt=this.options;
+            return {
+                x:opt.xMin+parseInt((opt.wrapWidth-opt.width-opt.xMin)*Math.random()),
+                y:opt.yMin+parseInt((opt.wrapHeight-opt.height-opt.yMin)*Math.random())
+            };
+        }
+    };
+
+    var play=function(){
+        var wrap=$('#wrap'),
+            hb=new HB({
+                wrap:wrap,
+                wrapWidth:wrap.width(),
+                wrapHeight:wrap.height(),
+                callback:function(a,index){
+                    alert('中奖了:'+index);
+
+                    setTimeout(function(){
+                        hb.play();
+                    },2000);
+
+                }
+            });
+
+        hb.play();
+    };
+    setTimeout(function(){
+        play();
+    },1);
+
+})();
