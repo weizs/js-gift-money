acb5270eb6c1cfbebdc50e18db9829999215dd50
weizs <weizs@mysoft.com.cn>
2015-08-27 17:08:42 +0800

    fixed bugs 遗留bug，部分手机端切换className不会播放动画

--- js/css3_gift_money.js
+++ js/css3_gift_money.js
@@ -1,4 +1,5 @@
 /**
+ * require jQuery or Zepto
  * Created by weizs on 2015/8/25.
  */
 (function($){
@@ -24,6 +25,12 @@
     };
 
     HB.prototype={
+        isSupportAnimateEnd:function(){
+            var element = document.body || document.documentElement,
+                animationEnd = element.style['WebkitAnimation']!=undefined ? 'webkitAnimationEnd':
+                    (element.style['animation']!=undefined?'animationend':false);
+            return animationEnd ? { end: animationEnd } : animationEnd;
+        },
         init:function(){
             var _self=this,
                 opt=_self.options;
@@ -33,15 +40,20 @@
 
             _self.wrap=$(opt.wrap);
 
+            _self.supportAnimationEnd=this.isSupportAnimateEnd();
+
             _self.wrap.on('click','.hb',function(){
                 var $this=$(this);
                 _self.stop();
                 opt.callback.call(this,$this,$this.attr('id').split('_')[1]);
             });
-            _self.wrap.on('webkitAnimationEnd animationend','.hb',function(){
+
+            _self.supportAnimationEnd && _self.wrap.on('webkitAnimationEnd animationend','.hb',function(){
                 var $this=$(this);
-                _self.setAnimateProp($this);
+                _self.calledAnimationEnd=true;
+
                 $this.css(_self.getPos());
+                _self.setAnimateProp($this);
                 $this.toggleClass('a1').toggleClass('a2');
             });
         },
@@ -54,26 +66,55 @@
                 _self.show();
         },
         setAnimateProp:function($dom){
-            var opt=this.options;
-            $dom.css({
-                '-webkit-animation-delay':(opt.minDelay+Math.random().toFixed(2)*opt.maxDelay)+'ms',
-                '-webkit-animation-duration':(opt.minDuration+Math.random().toFixed(2)*opt.maxDuration)+'ms'
-            });
+            var opt=this.options,
+                prop={
+                    delay:opt.minDelay+Math.random().toFixed(2)*opt.maxDelay,
+                    duration:opt.minDuration+Math.random().toFixed(2)*opt.maxDuration
+                };
+
+            if(this.supportAnimationEnd){
+                $dom.css({
+                    '-webkit-animation-delay':prop.delay+'ms',
+                    'animation-delay':prop.delay+'ms',
+                    '-webkit-animation-duration':prop.duration+'ms',
+                    'animation-duration':prop.duration+'ms'
+                });
+            }
+
+            return prop;
         },
         show:function(){
             var _self=this;
             var pos = _self.getPos(),
                 object = _self.getDom(pos);
 
-            _self.setAnimateProp(object.dom);
+            var prop = _self.setAnimateProp(object.dom);
             _self.wrap.append(object.dom);
+            _self.fix(object.dom,prop);
+        },
+        fix:function(dom,prop){
+            var _self=this;
+            if(_self.supportAnimationEnd){
+                setTimeout(function(){
+                    if(!_self.calledAnimationEnd)_self.supportAnimationEnd=false;
+                },prop.delay+prop.duration);
+            }else{
+                setTimeout(function(){
+                    dom.addClass('show');
+                },prop.delay);
+                setTimeout(function(){
+                    dom.removeClass('show');
+                    dom.css(_self.getPos());
+                    _self.fix(dom,_self.setAnimateProp(dom));
+                },prop.delay+prop.duration);
+            }
         },
         getDom:function(pos){
             var index=count++,
                 opt=this.options;
             pos.index=index;
             return {
-                dom:$('<div id="id_'+index+'" class="hb a1 '+(index%2?'r1':'r2')+'" style="left:'+pos.left+'px;top:'+pos.top+'px;width:'+opt.width+'px;height:'+opt.height+'px;line-height:'+opt.height+'px;"></div>'),
+                dom:$('<div id="id_'+index+'" class="hb'+(this.supportAnimationEnd?' a1':'')+' '+(index%2?'r1':'r2')+'" style="left:'+pos.left+'px;top:'+pos.top+'px;width:'+opt.width+'px;height:'+opt.height+'px;line-height:'+opt.height+'px;"></div>'),
                 id:index
             };
         },
